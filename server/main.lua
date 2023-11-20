ceb8ef1530c7c4e31bb1e9656266d35a7e6c0a34
Joe Szymkowicz <70592880+JoeSzymkowiczFiveM@users.noreply.github.com>
2023-11-19 21:57:16 -0600

    Replace thread and set dirt on server
    Replace thread with zone, and set dirt level on server.

--- server/main.lua
+++ server/main.lua
@@ -1,15 +1,17 @@
-local sharedConfig = require 'config.shared'
+local price = require 'config.shared'.price
 
 -- Events
-RegisterNetEvent('qbx_carwash:server:startWash', function()
-    local src = source
-    local player = exports.qbx_core:GetPlayer(src)
+RegisterNetEvent('qbx_carwash:server:startWash', function(netId)
+    local player = exports.qbx_core:GetPlayer(source)
 
     if not player then return end
 
-    if player.Functions.RemoveMoney('cash', sharedConfig.price, Lang:t('reason')) or player.Functions.RemoveMoney('bank', sharedConfig.price, Lang:t('reason')) then
-        TriggerClientEvent('qbx_carwash:client:washCar', src)
+    if player.Functions.RemoveMoney('cash', price, Lang:t('reason')) or player.Functions.RemoveMoney('bank', price, Lang:t('reason')) then
+        SetTimeout(6000, function()
+            SetVehicleDirtLevel(NetworkGetEntityFromNetworkId(netId), 0.0)
+        end)
+        TriggerClientEvent('qbx_carwash:client:washCar', player.PlayerData.source)
     else
-        exports.qbx_core:Notify(src, Lang:t('no_money'), 'error')
+        exports.qbx_core:Notify(player.PlayerData.source, Lang:t('no_money'), 'error')
     end
 end)
