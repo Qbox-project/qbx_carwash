f9b683a673e69a6a3139e8b0c7e857ec0c391347
Joe Szymkowicz <70592880+JoeSzymkowiczFiveM@users.noreply.github.com>
2023-11-20 22:47:53 -0600

    refactor: replace thread and set dirt on server
    * Replace thread and set dirt on server
    
    Replace thread with zone, and set dirt level on server.
    
    * fix: check on progress

--- client/main.lua
+++ client/main.lua
@@ -1,16 +1,15 @@
 -- Variables
 local config = require 'config.client'
-local sharedConfig = require 'config.shared'
-local washingVehicle = false
+local price = require 'config.shared'.price
 
 -- Events
 RegisterNetEvent('qbx_carwash:client:washCar', function()
-    washingVehicle = true
+    if source == '' then return end
     if lib.progressBar({
-        duration = math.random(4000, 8000),
+        duration = 6000,
         label = Lang:t('washing'),
         useWhileDead = false,
-        canCancel = true,
+        canCancel = false,
         disable = {
             move = true,
             car = true,
@@ -18,57 +17,45 @@ RegisterNetEvent('qbx_carwash:client:washCar', function()
             combat = true
         },
     }) then -- if completed
-        SetVehicleDirtLevel(cache.vehicle, 0.0)
-        SetVehicleUndriveable(cache.vehicle, false)
         WashDecalsFromVehicle(cache.vehicle, 1.0)
-        washingVehicle = false
     else -- if canceled
         exports.qbx_core:Notify(Lang:t('canceled'), 'error')
-        washingVehicle = false
     end
 end)
 
--- Threads
-CreateThread(function()
-    while true do
-        local playerCoords = GetEntityCoords(cache.ped)
-        local driver = cache.seat == -1
-        local dirtLevel = GetVehicleDirtLevel(cache.vehicle)
-        local sleep = 1000
-        if IsPedInAnyVehicle(cache.ped, false) then
-            for i = 1, #config.locations do
-                local dist = #(playerCoords - config.locations[i])
-                if dist <= 7.5 and driver then
-                    sleep = 0
-                    if not washingVehicle then
-                        DrawText3D(string.format('~g~E~w~ - Wash the car ($%s)', sharedConfig.price), config.locations[i])
-                        if IsControlJustPressed(0, 38) then
-                            if dirtLevel > config.dirtLevel then
-                                TriggerServerEvent('qbx_carwash:server:startWash')
-                            else
-                                exports.qbx_core:Notify(Lang:t('not_dirty'), 'error')
-                            end
-                        end
+local function createCarWashBlip(coords)
+    local carWash = AddBlipForCoord(coords.x, coords.y, coords.z)
+    SetBlipSprite(carWash, 100)
+    SetBlipDisplay(carWash, 4)
+    SetBlipScale(carWash, 0.75)
+    SetBlipAsShortRange(carWash, true)
+    SetBlipColour(carWash, 37)
+    BeginTextCommandSetBlipName('STRING')
+    AddTextComponentSubstringPlayerName(Lang:t('label'))
+    EndTextCommandSetBlipName(carWash)
+end
+
+for i=1, #config.locations do
+    local coords = config.locations[i]
+    createCarWashBlip(coords)
+    lib.zones.sphere({
+        coords = coords,
+        radius = 7.5,
+        -- debug = true,
+        inside = function()
+            if not lib.progressActive() then
+                DrawText3D(string.format('~g~E~w~ - Wash the car ($%s)', price), coords)
+                if IsControlJustPressed(0, 38) then
+                    if GetVehicleDirtLevel(cache.vehicle) > config.dirtLevel then
+                        local netId = NetworkGetNetworkIdFromEntity(cache.vehicle)
+                        TriggerServerEvent('qbx_carwash:server:startWash', netId)
                     else
-                        DrawText3D(Lang:t('not_available'), config.locations[i])
+                        exports.qbx_core:Notify(Lang:t('not_dirty'), 'error')
                     end
                 end
+            else
+                DrawText3D(Lang:t('not_available'), coords)
             end
-        end
-        Wait(sleep)
-    end
-end)
-
-CreateThread(function()
-    for i = 1, #config.locations do
-        local carWash = AddBlipForCoord(config.locations[i].x, config.locations[i].y, config.locations[i].z)
-        SetBlipSprite(carWash, 100)
-        SetBlipDisplay(carWash, 4)
-        SetBlipScale(carWash, 0.75)
-        SetBlipAsShortRange(carWash, true)
-        SetBlipColour(carWash, 37)
-        BeginTextCommandSetBlipName('STRING')
-        AddTextComponentSubstringPlayerName(Lang:t('label'))
-        EndTextCommandSetBlipName(carWash)
-    end
-end)
+        end,
+    })
+end
